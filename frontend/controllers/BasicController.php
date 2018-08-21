f6c256f92f7be2499ee38bdc22cec7de0570bde5
kostiv8888@gmail.com <kostiv8888@gmail.com>
2018-08-21 11:05:31 +0300

    add basic controller, layout, view

---
+++ frontend/controllers/BasicController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev
+ * Date: 21.08.18
+ * Time: 11:00
+ */
+
+namespace frontend\controllers;
+
+
+use yii\web\Controller;
+
+class BasicController extends Controller
+{
+    public $layout = 'basic';
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
