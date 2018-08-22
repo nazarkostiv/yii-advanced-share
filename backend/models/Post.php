4012b5f1979bf6fbef4ee0bc585edd0eccd0362e
kostiv8888@gmail.com <kostiv8888@gmail.com>
2018-08-22 14:57:07 +0300

    add Post model

---
+++ backend/models/Post.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ * @property string $date
+ * @property int $author
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'content', 'date', 'author'], 'required'],
+            [['content'], 'string'],
+            [['date'], 'safe'],
+            [['author'], 'integer'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+            'date' => 'Date',
+            'author' => 'Author',
+        ];
+    }
+}
