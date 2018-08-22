4b80eb7e5ede5b5c52e79ccb23ee90b159264a46
kostiv8888@gmail.com <kostiv8888@gmail.com>
2018-08-22 17:31:57 +0300

    little fix

--- backend/views/site/add-post.php
+++ backend/views/site/add-post.php
@@ -2,6 +2,8 @@
 
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
+
+$this->title = 'Add Post';
 ?>
 
 <div id="fh5co-contact">
@@ -10,15 +12,10 @@ use yii\helpers\Html;
 
             <?php
 
-            echo date('Y-m-d');
-            echo ' ';
-            echo Yii::$app->user->identity->username;
-
             $form = ActiveForm::begin();
 
             echo $form->field($model, 'title')->textInput(['autofocus' => true]);
             echo $form->field($model, 'content')->textarea(['rows' => 5]);
-//            echo $form->field($model, 'date');
 
             echo Html::submitButton('Send', ['class' => 'btn btn-success']);
 
